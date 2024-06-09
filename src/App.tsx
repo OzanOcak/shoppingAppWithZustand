524ea718336cc3fabc21bfcb96cfca66a04c7688
OzanOcak <ozanocak@gmail.com>
2024-06-09 16:34:42 -0400

    performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
-import { create } from "zustand";
-
-const useStore = create<{
-  count: number;
-  inc: () => void;
-  dec: () => void;
-}>((set) => ({
-  count: 0,
-  inc: () => set((state) => ({ count: state.count + 1 })),
-  dec: () => set((state) => ({ count: state.count - 1 })),
-}));
+import { useShallow } from "zustand/react/shallow";
+import { useStore } from "./store/store";
 
 function App() {
-  const store = useStore();
+  //const store = useStore();
+
+  //const age = useStore((state)=>state.age)  // good perfermonce
+  //const fullName = useStore((state)=>state,fullName)
+  //const address = useStore((state)=>state.address)
+
+  //but using shallow tidier
+
+  const { age, fullName, address } = useStore(
+    useShallow((state) => ({
+      age: state.age,
+      fullName: state.fullName,
+      address: state.address,
+    }))
+  );
   return (
     <>
-      <div className="text-center text-5xl">
-        <button onClick={store.inc}>+</button>
-        <Count />
-        <button onClick={store.dec}>-</button>
-      </div>
+      {/*<div className="text-center text-xl">{JSON.stringify(store)}</div>*/}
+      <div>{address}</div>
     </>
   );
-}
-
-function Count() {
-  const store = useStore();
-  return <p className="text-black text-center text-xl">{store.count}</p>;
 }
 
 export default App;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import { create } from "zustand";
 import { createUserSlice, UserSlice } from "./user-slice";
 import { immer } from "zustand/middleware/immer";
+import { CartSlice, createCartSlice } from "./cart-slice";
 
-export type Store = UserSlice;
+export type Store = UserSlice & CartSlice;
 
 export const useStore = create<Store>()(immer((...args)=>({
-    ...createUserSlice(...args)
+    ...createUserSlice(...args),
+    ...createCartSlice(...args),
 })
 ))
