46a3310f0ebedb16e9d4e75038585e45a3bda3c5
OzanOcak <ozanocak@gmail.com>
2024-06-09 21:50:18 -0400

    persist middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,15 +2,18 @@ import { create } from "zustand";
 import { createUserSlice, UserSlice } from "./user-slice";
 import { immer } from "zustand/middleware/immer";
 import { CartSlice, createCartSlice } from "./cart-slice";
-import { subscribeWithSelector } from "zustand/middleware";
+import { persist, subscribeWithSelector } from "zustand/middleware";
 
 export type Store = UserSlice & CartSlice;
 
 export const useStore = create<Store>()(
-    subscribeWithSelector(
-        immer((...args)=>({
+    persist(
+        subscribeWithSelector(
+          immer((...args)=>({
             ...createUserSlice(...args),
             ...createCartSlice(...args),
-        })
-    ))
+          })),
+        ),
+        {name:'local-storage'}
+    )
 )
